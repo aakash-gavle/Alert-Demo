7ec7c58507c4c060c08203e659188eed0b094ccb
aakash-gavle <gavleaakash@gmail.com>
2020-05-06 17:28:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/alertdemo/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.alertdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    TextView textView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+         super.onOptionsItemSelected(item);
+
+         switch (item.getItemId()){
+             case R.id.english:
+                 setLanguage("English");
+                 return true;
+             case R.id.spanish:
+                 setLanguage("Spanish");
+                 return true;
+             default:
+                 return false;
+         }
+
+    }
+
+    public void setLanguage(String language){
+
+        sharedPreferences.edit().putString("language",language).apply();
+
+        textView.setText(language);
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       sharedPreferences=this.getSharedPreferences("com.example.alertdemo",Context.MODE_PRIVATE);
+       textView=findViewById(R.id.textView);
+
+       String language =sharedPreferences.getString("language","Error");
+        Log.i("THE LANG",language);
+
+      if (language.equals("Error")){
+          new AlertDialog.Builder(this)
+                  .setIcon(android.R.drawable.ic_btn_speak_now)
+                  .setTitle("Chose a language")
+                  .setMessage("Which language you like to chose")
+                  .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                      @Override
+                      public void onClick(DialogInterface dialogInterface, int i) {
+                          //set English
+                          setLanguage("English");
+                      }
+                  })
+
+                  .setNegativeButton("Spanish", new DialogInterface.OnClickListener() {
+                      @Override
+                      public void onClick(DialogInterface dialogInterface, int i) {
+                          //set spanish
+                          setLanguage("Spanish");
+                      }
+                  })
+                  .show();
+      }else{
+          textView.setText(language);
+      }
+
+    }
+}
